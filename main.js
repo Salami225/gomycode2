cb6667e61df6b5fae3cf450a9aa3691f2400f05d
Salami225 <salamiabdoulahi@gmail.com>
2025-07-10 12:00:35 +0000

    mon premier commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// console.log('bonjour monsieur')
+
+function salami(nb1,nb2){
+    return nb1%nb2
+    
+}
+console.debug("bonjour madame"+" "+"la directrice de gomycode")
+console.log("bonjour madame"+" "+"la directrice de gomycode")
+alert("bonjour")
